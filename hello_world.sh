1afd0ee5f388d1009fd38b365bb811335eed700b
augie <augie@augies-MacBook-Pro.local>
2020-01-12 19:31:37 -0800

    Hello World Bash Script
    This program is very happy to exist.

---
+++ hello_world.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Note: Change file permissions as needed to run script.
+# For UNIX/Linux systems run 'chmod 755 hello_world.sh'
+# before executing the file.
+
+echo "Hello world!"
